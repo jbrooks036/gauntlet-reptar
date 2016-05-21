ccfb861dc54bbbae916782544823cdb8cfb4555c
pbhowelljr <pbhowelljr@gmail.com>
2016-05-20 20:50:33 -0500

    add combat.js file

--- javascripts/constructors.js
+++ javascripts/constructors.js
@@ -1,57 +1,63 @@
 "use strict";
 
-//COMBATANT CONSTRUCTOR FUNCTION
-var Combatant = function(){
-  this.health = 100;
-  this.damage = 10;
-};
+var gauntlet = (function(object){
 
-// FEDERATION (RACE 1) CONSTRUCTOR FUNCTION
-var Federation = function(){
-  this.raceHealthModifier = 1.4;
-  this.raceDamageModifier = 1.1;
-};
+  //COMBATANT CONSTRUCTOR FUNCTION
+  object.Combatant = function(){
+    this.health = 100;
+    this.damage = 10;
+  };
 
-// KLINGON (RACE 2) CONSTRUCTOR FUNCTION
-var Klingon = function(){
-  this.raceHealthModifier = 1.1;
-  this.raceDamageModifier = 1.4;
-};
+  // FEDERATION (RACE 1) CONSTRUCTOR FUNCTION
+  object.Federation = function(){
+    this.raceHealthModifier = 1.4;
+    this.raceDamageModifier = 1.1;
+  };
 
-// CRUISER (CLASS 1) CONSTRUCTOR FUNCTION
-var Cruiser = function(){
-  this.classHealthModifier = 1.2;
-  this.classDamageModifier = 1.3;
-};
+  // KLINGON (RACE 2) CONSTRUCTOR FUNCTION
+  object.Klingon = function(){
+    this.raceHealthModifier = 1.1;
+    this.raceDamageModifier = 1.4;
+  };
 
-// DESTROYER (CLASS 2) CONSTRUCTOR FUNCTION
-var Destroyer = function(){
-  this.classHealthModifier = 1;
-  this.classDamageModifier = 1.5;
-};
+  // CRUISER (CLASS 1) CONSTRUCTOR FUNCTION
+  object.Cruiser = function(){
+    this.classHealthModifier = 1.2;
+    this.classDamageModifier = 1.3;
+  };
 
-//LASER (WEAPON 1) CONSTRUCTOR FUNCTION
-var Laser = function(){
-  this.weaponDamageModifier = 1.2;
-};
+  // DESTROYER (CLASS 2) CONSTRUCTOR FUNCTION
+  object.Destroyer = function(){
+    this.classHealthModifier = 1;
+    this.classDamageModifier = 1.5;
+  };
 
-//PHOTON TORPEDO (WEAPON 2) CONSTRUCTOR FUNCTION
-var Photon = function(){
-  this.weaponDamageModifier = 1.2;
-};
+  //LASER (WEAPON 1) CONSTRUCTOR FUNCTION
+  object.Laser = function(){
+    this.weaponDamageModifier = 1.2;
+  };
 
-//PULSE CANNON (WEAPON 3) CONSTRUCTOR FUNCTION
-var Pulse = function(){
-  this.weaponDamageModifier = 1.2;
-};
+  //PHOTON TORPEDO (WEAPON 2) CONSTRUCTOR FUNCTION
+  object.Photon = function(){
+    this.weaponDamageModifier = 1.2;
+  };
 
-// PLAYER CONSTRUCTOR FUNCTION
-var Player = function(name){
-  this.name = name;
-};
+  //PULSE CANNON (WEAPON 3) CONSTRUCTOR FUNCTION
+  object.Pulse = function(){
+    this.weaponDamageModifier = 1.2;
+  };
 
-// ENEMEY CONSTRUCTOR FUNCTION
-var Enemy = function(){
-  this.enemyHealthPenalty = 0.9;
-};
+  // PLAYER CONSTRUCTOR FUNCTION
+  object.Player = function(name){
+    this.name = name;
+  };
+
+  // ENEMEY CONSTRUCTOR FUNCTION
+  object.Enemy = function(){
+    this.name = 'Enemy Vessel'
+    this.enemyHealthPenalty = 0.9;
+  };
+
+  return object;
+})(gauntlet || {});
 
