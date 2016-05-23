ce00162312ec7f80b5166045a1914a5851061f31
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-05-23 14:37:19 -0500

    added comments

--- javascripts/enemyGenerator.js
+++ javascripts/enemyGenerator.js
@@ -1,19 +1,24 @@
-"use strict";
+//*****ENEMY GENERATOR*****//
+'use strict';
 
 var gauntlet = (function(obj){
+	// ARRAYS OF POSSIBLE ENEMY RACES, CLASSES, AND WEAPONS //
 	var allowedRaces = ["Federation", "Klingon"];
 	var allowedClasses = ["Cruiser", "Destroyer"];
 	var allowedWeapons = ["Laser", "Photon", "Pulse"];
 
+	// FUNCTION TO GET A RANDOM INDEX FROM EACH OF THE ABOVE ARRAYS //
 	var generateConstructorIndexes = function(min, max) {
 		return Math.floor(Math.random() * (max - min)) + min;
 	};
 
+	// FUNCTION TO CALL RANDOM NUMBER FUNCTION ABOVE //
+	// CHAIN THE RESULTS TO GAUNTLET.ENEMY //
 	obj.randomEnemy = function(){
-
 	  var randomRace = allowedRaces[generateConstructorIndexes(0, allowedRaces.length)];
 	  var randomClass = allowedClasses[generateConstructorIndexes(0, allowedClasses.length)];
 	  var randomWeapon = allowedWeapons[generateConstructorIndexes(0, allowedWeapons.length)];
+
 	  gauntlet[randomRace].prototype = new obj.Combatant();
 	  gauntlet[randomClass].prototype = new gauntlet[randomRace]();
 	  gauntlet[randomWeapon].prototype = new gauntlet[randomClass]();
@@ -21,7 +26,7 @@ var gauntlet = (function(obj){
 
 	  return new obj.Enemy();
 	};
-
+	// RETURN NEW ENEMY //
 	return obj;
 
 })(gauntlet || {});
